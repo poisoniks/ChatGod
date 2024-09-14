1dfb5ca4d09fcb5334c7dfa1f9abfc92d6af5502
Vladislav Dremkov <vladremkov888@gmail.com>
2024-09-14 20:00:00 +0300

    Create a factory to handle dependencies

---
+++ src/main/java/com/poisoniks/chatgod/util/Factory.java
@@ -0,0 +1,45 @@
+package com.poisoniks.chatgod.util;
+
+import com.poisoniks.chatgod.Config;
+import com.poisoniks.chatgod.ai.GPTConnector;
+import com.poisoniks.chatgod.entity.ChatGodThread;
+import com.poisoniks.chatgod.service.AIService;
+import com.poisoniks.chatgod.service.ChatManager;
+import com.poisoniks.chatgod.service.impl.AIServiceImpl;
+import com.poisoniks.chatgod.service.impl.ChatManagerImpl;
+
+public class Factory {
+    private static final AIService aiService;
+    private static final ChatManager chatManager;
+    private static final ChatGodThread chatGodThread;
+    private static final GPTConnector gptConnector;
+    private static final ChatHelper chatHelper;
+
+    static {
+        gptConnector = new GPTConnector();
+        chatManager = new ChatManagerImpl();
+        chatHelper = new ChatHelper();
+        aiService = new AIServiceImpl(chatManager, gptConnector, chatHelper);
+        chatGodThread = new ChatGodThread(Config.rate, aiService);
+    }
+
+    public static AIService getAiService() {
+        return aiService;
+    }
+
+    public static ChatManager getChatManager() {
+        return chatManager;
+    }
+
+    public static ChatGodThread getChatGodThread() {
+        return chatGodThread;
+    }
+
+    public static GPTConnector getGptConnector() {
+        return gptConnector;
+    }
+
+    public static ChatHelper getChatHelper() {
+        return chatHelper;
+    }
+}
