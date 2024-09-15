285a529c0c4997485e023d7897e4d04789d35772
Vladislav Dremkov <vladremkov888@gmail.com>
2024-09-15 15:42:35 +0300

    Make the mod server-side only

--- src/main/java/com/poisoniks/chatgod/ChatGod.java
+++ src/main/java/com/poisoniks/chatgod/ChatGod.java
@@ -10,7 +10,7 @@ import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.event.FMLServerStartingEvent;
 
-@Mod(modid = ChatGod.MODID, version = Tags.VERSION, name = "ChatGod", acceptedMinecraftVersions = "[1.7.10]")
+@Mod(modid = ChatGod.MODID, version = Tags.VERSION, name = "ChatGod", acceptedMinecraftVersions = "[1.7.10]", acceptableRemoteVersions = "*")
 public class ChatGod {
 
     public static final String MODID = "chatgod";
